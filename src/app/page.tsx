eb82e0b3cb51af4f35d58936f2042a28098d1777
Alastair Swan <alastair1008@gmail.com>
2025-01-20 20:45:20 -0800

    setup next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Client from "./clientapp";
+
+export default function Home() {
+
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Client/>
+      </main>
+      <footer className={styles.footer}>
+        
+      </footer>
+    </div>
+  );
+}
