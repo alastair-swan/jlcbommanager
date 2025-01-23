7ece0d19a484ef935397577520095a8cd604a70a
Alastair Swan <alastair1008@gmail.com>
2025-01-22 20:14:28 -0800

    improved the ability to parse malformed files

--- src/app/clientapp/parsers/fusioncsv.tsx
+++ src/app/clientapp/parsers/fusioncsv.tsx
@@ -2,39 +2,75 @@ import { BOMPart, BOMItem } from "../bomtypes"
 
 
 export function parseFusionCSV(bomfile: string, pnpfront: string, pnpback: string, pnpheaders: true|Array<string> = ["identifier", "x", "y", "rotation", "value", "partnumber"]): Array<BOMPart>{
+    
+    if (bomfile.startsWith("Designator"))
+        pnpheaders = ["Designator","Mid X","Mid Y","Layer","Rotation"]
+    
     const bomdata = CSVToArray(bomfile)
     const pnpf = CSVToArray(pnpfront, pnpheaders)
     const pnpb = CSVToArray(pnpback, pnpheaders)
+
+    //console.log(pnpf)
 
     const pnp: Array<BOMItem> = []
     const bom: Array<BOMPart> = []
 
     for (var frontpnpindex = 0; frontpnpindex < pnpf.length; frontpnpindex++){
+        var l
+        if (pnpf[frontpnpindex].layer != undefined)
+            l = pnpf[frontpnpindex].layer
+        else if (pnpf[frontpnpindex].Layer != undefined)
+            l = (pnpf[frontpnpindex].Layer != undefined)
+        if (l === "Bottom" || l === "Back")
+            l = "Bottom"
+        else
+            l = "Top"
+
+        var x = pnpf[frontpnpindex].x != undefined ? pnpf[frontpnpindex].x : pnpf[frontpnpindex]["Mid X"]
+        var y = pnpf[frontpnpindex].y != undefined ? pnpf[frontpnpindex].y : pnpf[frontpnpindex]["Mid Y"]
+        var r = pnpf[frontpnpindex].rotation != undefined ? pnpf[frontpnpindex].rotation : pnpf[frontpnpindex].Rotation
+        
         pnp.push(
             {
-                id: pnpf[frontpnpindex].identifier,
-                x: {value: pnpf[frontpnpindex].x, default: pnpf[frontpnpindex].x},
-                y: {value: pnpf[frontpnpindex].y, default: pnpf[frontpnpindex].y},
-                rotation: { value: pnpf[frontpnpindex].rotation, default: pnpf[frontpnpindex].rotation}, 
-                layer: {value: pnpf[frontpnpindex].layer != undefined ? pnpf[frontpnpindex].layer : "top", default: pnpf[frontpnpindex].layer != undefined ? pnpf[frontpnpindex].layer : "top"}
+                id: pnpf[frontpnpindex].identifier != undefined ? pnpf[frontpnpindex].identifier : pnpf[frontpnpindex].Designator,
+                x: {value: x, default: x},
+                y: {value: y, default: y},
+                rotation: { value: r, default: r}, 
+                layer: {value: l as "Top"|"Bottom", default: l as "Top"|"Bottom"},
+                hasPNP: true
             }
         )
     }
     for (var backpnpindex = 0; backpnpindex < pnpb.length; backpnpindex++){
+
+        var l
+        if (pnpb[backpnpindex].layer != undefined)
+            l = pnpb[backpnpindex].layer
+        else if (pnpb[backpnpindex].Layer != undefined)
+            l = (pnpb[backpnpindex].Layer != undefined)
+        if (l === "Top" || l === "Front")
+            l = "Top"
+        else
+            l = "Bottom"
+
+        var x = pnpb[backpnpindex].x != undefined ? pnpb[backpnpindex].x : pnpb[backpnpindex]["Mid X"]
+        var y = pnpb[backpnpindex].y != undefined ? pnpb[backpnpindex].y : pnpb[backpnpindex]["Mid Y"]
+        var r = pnpb[backpnpindex].rotation != undefined ? pnpb[backpnpindex].rotation : pnpb[backpnpindex].Rotation
         pnp.push(
             {
-                id: pnpb[backpnpindex].identifier,
-                x: { value: pnpb[backpnpindex].x, default: pnpb[backpnpindex].x },
-                y: { value: pnpb[backpnpindex].y, default: pnpb[backpnpindex].y },
-                rotation: { value: pnpb[backpnpindex].rotation, default: pnpb[backpnpindex].rotation },
-                layer: {value: pnpb[backpnpindex].layer != undefined ? pnpb[backpnpindex].layer : "back", default: pnpb[backpnpindex].layer != undefined ? pnpb[backpnpindex].layer : "back"}
+                id: pnpb[backpnpindex].identifier != undefined ? pnpb[backpnpindex].identifier : pnpb[backpnpindex].Designator,
+                x: { value: x, default: x },
+                y: { value: y, default: y },
+                rotation: { value: r, default: r },
+                layer: {value: l as "Top"|"Bottom", default: l as "Top"|"Bottom"},
+                hasPNP: true
             }
         )
     }
 
     for (var bomindex = 0; bomindex < bomdata.length; bomindex++){
         const pn = bomdata[bomindex].Device != undefined ? bomdata[bomindex].Device : bomdata[bomindex].Footprint
-        const pac = bomdata[bomindex].Package
+        const pac = bomdata[bomindex].Package != undefined ? bomdata[bomindex].Package : bomdata[bomindex].Footprint
         const val = bomdata[bomindex].Value != undefined ? bomdata[bomindex].Value : bomdata[bomindex].Comment
         const JLC = bomdata[bomindex]["JLCPCB Part #"] != undefined ? bomdata[bomindex]["JLCPCB Part #"] : ""
         var itemlist = bomdata[bomindex].Parts != undefined ? bomdata[bomindex].Parts : bomdata[bomindex].Designator
@@ -43,11 +79,24 @@ export function parseFusionCSV(bomfile: string, pnpfront: string, pnpback: strin
             itemlist = [itemlist]
         }
         for (var i = 0; i < itemlist.length; i++){
+            var matchfound = false
             for (var j = 0; j < pnp.length; j++){
                 if (itemlist[i] === pnp[j].id){
                     items.push(pnp[j])
+                    pnp[j].inUse = true
+                    matchfound = true
                     continue
                 }
+            }
+            if (!matchfound){
+                items.push({
+                    id: itemlist[i],
+                    x: {value: "unknown", default: "unknown"},
+                    y: {value: "unknown", default: "unknown"},
+                    rotation: {value: "unknown", default: "unknown"},
+                    layer: {value: undefined, default: undefined},
+                    hasPNP: false
+                })
             }
         }
         
@@ -58,6 +107,24 @@ export function parseFusionCSV(bomfile: string, pnpfront: string, pnpback: strin
             value: {value: val, default: val},
             JLC: {value: JLC, default: JLC},
             items: {value: items, default: items}
+        })
+    }
+
+    const unusedPNP: BOMItem[] = []
+    pnp.map((d, i) => {
+        if (!d.inUse && d.id != "Designator"){
+            unusedPNP.push(d)
+        }
+    })
+
+    if (unusedPNP.length != 0){
+        bom.push({
+            id: bom.length,
+            partnumber: {value: "unknown", default: "unknown"},
+            package: {value: "unknown", default: "unknown"},
+            value: {value: "unknown", default: "unknown"},
+            JLC: {value: "unknown", default: "unknown"},
+            items: {value: unusedPNP, default: unusedPNP},
         })
     }
 
@@ -131,12 +198,12 @@ function CSVHeaderToArray(line: string): Array<string>{
 }
 
 function CSVLineToArray(line: string): CSVItem{
-    const pattern = RegExp('(?:(?:"(.*?)"|(.*?))(?:,|$))', 'gy')
+    const pattern = RegExp('(?:(?:"(.*?)"|(.*?))(?:,|$|\\r))', 'gy')
     const partlistpatern = RegExp(',')
     const linearray = Array<CSVItem>()
 
     var dataitemarray = null
-    const maxitems = 100
+    const maxitems = 200
     while ( !(linearray.length >= maxitems || pattern.lastIndex === line.length) && (dataitemarray = pattern.exec(line))){
         var dataitem: CSVItem = ""
         for (var i = dataitemarray.length-1; i >= 0; i--){
@@ -153,6 +220,5 @@ function CSVLineToArray(line: string): CSVItem{
         }
         linearray.push(dataitem)
     }
-
     return linearray
 }
